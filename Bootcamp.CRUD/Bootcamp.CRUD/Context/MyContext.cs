8a16c1efd64a5f5460deffb57b6766bdfb97a4db
IqbalAula <iqbalaularahman@gmail.com>
2019-01-23 17:35:14 +0700

    Ini Program CRUD

---
+++ Bootcamp.CRUD/Bootcamp.CRUD/Context/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Bootcamp.CRUD.Context.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.CRUD.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("bootcamp22") { }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
+
